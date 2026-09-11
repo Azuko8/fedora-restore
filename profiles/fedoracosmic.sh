8b1354882de80c56fd7aa87ce03ad006d6706ccb
azuko <88248386+Azuko8@users.noreply.github.com>
2026-09-11 00:47:24 +0000

    add localsend

--- profiles/fedoracosmic.sh
+++ profiles/fedoracosmic.sh
@@ -76,6 +76,7 @@ if flatpak install -y flathub \
     it.mijorus.gearlever \
     net.davidotek.pupgui2 \
     org.fedoraproject.MediaWriter \
+    org.localsend.localsend_app \
     org.prismlauncher.PrismLauncher; then
     echo "Flatpak packages installed successfully."
 else
