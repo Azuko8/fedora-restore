c510f27f858b359c82545ba0360a5172377e72d4
azuko <88248386+Azuko8@users.noreply.github.com>
2026-09-26 00:21:48 +0000

    fix formatting

--- profiles/fedoraws.sh
+++ profiles/fedoraws.sh
@@ -76,8 +76,7 @@ if flatpak install -y flathub \
     com.mattjakeman.ExtensionManager \
     org.localsend.localsend_app \
     org.prismlauncher.PrismLauncher \
-    com.github.tchx84.Flatseal \
-    > /dev/null; then
+    com.github.tchx84.Flatseal > /dev/null; then
     echo "Flatpak packages installed successfully."
 else
     echo "Error: Flatpak installation failed."
